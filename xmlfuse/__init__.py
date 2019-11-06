bc5cb0cc7d1a4b05930352272a043a2155dde9f2
Mike Kroutikov <mkroutikov@innodata.com>
2019-11-06 14:17:16 -0500

    hooking in travis CI

--- xmlfuse/__init__.py
+++ xmlfuse/__init__.py
@@ -1,4 +1,4 @@
-__version__      = '0.0.1'
+__version__      = '0.0.2'
 __description__  = 'Fuses two XML markups together'
 __url__          = 'https://github.com/innodatalabs/xmlfuse'
 __author__       = 'Mike Kroutikov'
